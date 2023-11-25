c08cddab4dbb3c68defffd7d2fca357c99934888
labankibet <laban.kibet@moringaschool.com>
2023-11-25 18:18:14 +0300

    updating auth values

--- inventory/src/Login.js
+++ inventory/src/Login.js
@@ -8,6 +8,10 @@ function Login() {
         password: ''
     })
 
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+
     const handleSubmit =(event) => {
         event.preventDefault();
     }
@@ -19,12 +23,12 @@ function Login() {
             <form action="" onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' 
+                    <input type="email" placeholder='Enter Email' name='email'
                     onChange={handleInput} className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' 
+                    <input type="password" placeholder='Enter Password' name='password' 
                     onChange={handleInput} className='form-control rounded-0'/>
                 </div>
                 <button type= 'submit' className='btn btn-success w-100 rounded-0'><strong>Login</strong></button>
