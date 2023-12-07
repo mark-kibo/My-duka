d710bdd86805fed364be18cf7d59e55560aaa07e
masibo <vicsibo01@gmail.com>
2023-12-07 14:23:01 +0000

    updated stores with api

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -1,26 +1,97 @@
 import React, { useState } from 'react';
+import {
+  useQuery,
+  useMutation,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
 
-const Stores = () => {
-  const [storeData, setStoreData] = useState([
-    { id: 1, name: 'Store 1', location: 'Location 1' },
-    { id: 2, name: 'Store 2', location: 'Location 2' },
-    { id: 3, name: 'Store 3', location: 'Location 3' },
-    { id: 4, name: 'Store 4', location: 'Location 4' },
-    { id: 5, name: 'Store 4', location: 'Location 4' },
-    { id: 6, name: 'Store 5', location: 'Location 5' },
-    { id: 7, name: 'Store 6', location: 'Location 6' },
-    { id: 8, name: 'Store 7', location: 'Location 7' },
-    { id: 9, name: 'Store 8', location: 'Location 8' },
-    { id: 10, name: 'Store 9', location: 'Location 9' },
-    { id: 11, name: 'Store 10', location: 'Location 10' },
-    { id: 12, name: 'Store 11', location: 'Location 11' },
-    { id: 13, name: 'Store 12', location: 'Location 12' },
-    { id: 14, name: 'Masibo', location: 'Location 13' },
-    // ... (more store data)
-  ]);
 
+ // Define queryClient
+  const queryClient = new QueryClient();
+  const Stores = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [newStore, setNewStore] = useState({ name: '', location: '' });
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+
+ 
+
+  const { data: storeData, isLoading, isError, refetch } = useQuery(
+    'stores',
+    async () => {
+      const response = await fetch('https://myduka-apis.onrender.com/stores/');
+      if (!response.ok) {
+        throw new Error('Failed to fetch stores');
+      }
+      const data = await response.json();
+      return data;
+    }
+  );
+
+  const addStoreMutation = useMutation(
+    async (newStore) => {
+      const response = await fetch('https://myduka-apis.onrender.com/stores/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newStore),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to add a new store');
+      }
+      return response.json();
+    },
+    {
+      onSuccess: (data) => {
+        // Ensure that refetch is defined before calling it
+        if (refetch) {
+          refetch();
+        }
+        closeModal();
+      },
+      onError: (error) => {
+        console.error('Mutation failed with error:', error);
+      },
+    }
+  );
+  
+  
+
+  // Filtering logic
+  const filteredStores =
+    storeData?.filter((store) =>
+      store.store_name &&
+      store.store_name.toLowerCase().includes(searchTerm.toLowerCase())
+    ) || [];
+
+  // Pagination logic
+  const storesPerPage = 5;
+  const indexOfLastStore = currentPage * storesPerPage;
+  const indexOfFirstStore = indexOfLastStore - storesPerPage;
+  const currentStores = filteredStores.slice(
+    indexOfFirstStore,
+    indexOfLastStore
+  );
+
+  // Function to handle pagination
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // Function to handle search
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1); // Reset to the first page when searching
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
@@ -32,32 +103,15 @@ const Stores = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setStoreData([...storeData, { id: storeData.length + 1, ...newStore }]);
-    closeModal();
+    addStoreMutation.mutate(newStore);
   };
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [storesPerPage] = useState(5);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  // Filtering logic
-  const filteredStores = storeData.filter((store) =>
-    store.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  // Pagination logic
-  const indexOfLastStore = currentPage * storesPerPage;
-  const indexOfFirstStore = indexOfLastStore - storesPerPage;
-  const currentStores = filteredStores.slice(indexOfFirstStore, indexOfLastStore);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="p-8">
       <h1 className="text-3xl font-extrabold mb-6">MyDuka Stores</h1>
 
-       {/* Add New Store button */}
-       <button
+      {/* Add New Store button */}
+      <button
         onClick={openModal}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none"
       >
@@ -68,7 +122,7 @@ const Stores = () => {
       <input
         type="text"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(e) => handleSearch(e)}
         placeholder="Search for a store..."
         className="mt-4 p-2 border rounded"
       />
@@ -84,52 +138,15 @@ const Stores = () => {
         </thead>
         <tbody>
           {currentStores.map((store) => (
-            <tr key={store.id}>
-              <td className="border p-2">{store.id}</td>
-              <td className="border p-2">{store.name}</td>
+            <tr key={store.store_id}>
+              <td className="border p-2">{store.store_id}</td>
+              <td className="border p-2">{store.store_name}</td>
               <td className="border p-2">{store.location}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      {/* Pagination */}
-      <div className="flex justify-end mt-4">
-        <span>
-          Page{' '}
-          <strong>
-            {currentPage} of {Math.ceil(filteredStores.length / storesPerPage)}
-          </strong>{' '}
-        </span>
-        <button
-          onClick={() => paginate(1)}
-          className="ml-2 text-blue-500"
-          disabled={currentPage === 1}
-        >
-          {'<<'}
-        </button>
-        <button
-          onClick={() => paginate(currentPage - 1)}
-          className="ml-2 text-blue-500"
-          disabled={currentPage === 1}
-        >
-          {'<'}
-        </button>
-        <button
-          onClick={() => paginate(currentPage + 1)}
-          className="ml-2 text-blue-500"
-          disabled={currentPage === Math.ceil(filteredStores.length / storesPerPage)}
-        >
-          {'>'}
-        </button>
-        <button
-          onClick={() => paginate(Math.ceil(filteredStores.length / storesPerPage))}
-          className="ml-2 text-blue-500"
-          disabled={currentPage === Math.ceil(filteredStores.length / storesPerPage)}
-        >
-          {'>>'}
-        </button>
-      </div>
       {/* Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
@@ -172,8 +189,52 @@ const Stores = () => {
           </div>
         </div>
       )}
+
+      {/* Pagination */}
+      <div className="flex justify-end mt-4">
+        <button
+          onClick={() => paginate(1)}
+          className="ml-2 text-blue-500"
+          disabled={currentPage === 1}
+        >
+          {'<<'}
+        </button>
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          className="ml-2 text-blue-500"
+          disabled={currentPage === 1}
+        >
+          {'<'}
+        </button>
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          className="ml-2 text-blue-500"
+          disabled={currentPage === Math.ceil(filteredStores.length / storesPerPage)}
+        >
+          {'>'}
+        </button>
+        <button
+          onClick={() => paginate(Math.ceil(filteredStores.length / storesPerPage))}
+          className="ml-2 text-blue-500"
+          disabled={currentPage === Math.ceil(filteredStores.length / storesPerPage)}
+        >
+          {'>>'}
+        </button>
+        <span>
+          Page{' '}
+          <strong>
+            {currentPage} of {Math.ceil(filteredStores.length / storesPerPage)}
+          </strong>{' '}
+        </span>
+      </div>
     </div>
   );
 };
 
-export default Stores;
+const StoresWrapper = () => (
+  <QueryClientProvider client={queryClient}>
+    <Stores />
+  </QueryClientProvider>
+);
+
+export default StoresWrapper;
