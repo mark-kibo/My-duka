8c51f7fafbcc098bc8bd5f8183656cede4aa1f7f
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-12-07 18:09:41 +0300

    Add the updated files

--- src/router/Router.js
+++ src/router/Router.js
@@ -6,6 +6,8 @@ import New from '../components/new'
 import Stores from '../components/Stores'
 import PrivateRoute from '../utils/PrivateRoute'
 import Login from '../components/Login'
+import UserTable from '../components/UserTable'
+
 
 const Router = () => {
     return (
@@ -14,12 +16,15 @@ const Router = () => {
                 <Routes>
                 <Route element={<PrivateRoute />}>
                     <Route path="/" element={<App />}>
-                        <Route index element={<Main />} />
+                        <Route index element={<Main userRole={'merchant'}/>} />
                         <Route path='/stores' element={<Stores />} />
+                        <Route path='/users' element={<UserTable />} />
+                        
 
                     </Route>
                 </Route>
                 <Route path='/login' element={<Login/>}/>
+                
                 </Routes>
             </BrowserRouter>
         </div>
